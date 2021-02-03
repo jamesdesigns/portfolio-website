cf091b2968901c8afbf4bc648aaf2f7844c95280
James Hooper <jameshooperdesigns@gmail.com>
2021-02-03 11:01:54 -0700

    Add react-router-dom and add the Link tag to the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,11 @@ function Navbar() {
   return (
     <>
       <nav className="navbar">
-        
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            
+          </Link>
+        </div>
       </nav>
     </>
   )
