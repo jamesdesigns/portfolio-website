53fa9aaac5b2698bab5a34983efc8b2190118d0c
James Hooper <jameshooperdesigns@gmail.com>
2021-02-03 14:45:22 -0700

    Replace Logo Icon with artstation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          <i className="fab fa-typo3"></i>JH 
+          <i className="fab fa-artstation"></i>&nbsp; JH 
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
