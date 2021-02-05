704682def011833a08adcf57fdfba21ba6e0d859
James Hooper <jameshooperdesigns@gmail.com>
2021-02-04 17:45:22 -0700

    Add an extra CardItem component with description

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,8 +11,14 @@ function Cards() {
           <ul className="cards_items">
             <CardItem
               src="images/img-9.jpg"
-              text="Tip Calculator"
-              label="Logic"
+              text="Tip Calculator that works as a tip and pay using the QR Scan code."
+              label="Tip Calculator App"
+              path="/portfolio"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              label="Restuarant Website"
               path="/portfolio"
             />
           </ul>
