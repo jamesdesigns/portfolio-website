5c65686f242a6d09c860433445d22423fb9a9343
James Hooper <jameshooperdesigns@gmail.com>
2021-02-05 22:42:46 -0700

    Add extra CardItem components to the Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import CardItem from './CardItem'
 import './Cards.css'
+import CardItem from './CardItem'
+
 
 function Cards() {
   return (
@@ -8,11 +9,31 @@ function Cards() {
       <h1>Portfolio</h1>
       <div className="cards__contianer">
         <div className="cards__wrapper">
-          <ul className="cards_items">
+          <ul className="cards__items">
             <CardItem
               src="images/img-9.jpg"
               text="Tip Calculator that works as a tip and pay using the QR Scan code."
               label="Tip Calculator App"
+              path="/portfolio"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              label="Restuarant Website"
+              path="/portfolio"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Tip Calculator that works as a tip and pay using the QR Scan code."
+              label="Tip Calculator App"
+              path="/portfolio"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              label="Restuarant Website"
               path="/portfolio"
             />
             <CardItem
