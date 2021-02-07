9488aa89760fa6172ab4670678d15afd0611a10e
James Hooper <jameshooperdesigns@gmail.com>
2021-02-06 21:06:26 -0700

    Update class names for each page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,5 +2,5 @@ import React from 'react'
 import '../../App.css'
 
 export default function About() {
-  return <h1 className="products">ABOUT</h1>;
+  return <h1 className="about">ABOUT</h1>;
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,5 +2,5 @@ import React from 'react'
 import '../../App.css'
 
 export default function Portfolio() {
-  return <h1 className="products">PORTFOLIO</h1>;
+  return <h1 className="portfolio">PORTFOLIO</h1>;
 }
