2ce42b08c8b90062bc20aa184fd38d637f0cb63f
James Hooper <jameshooperdesigns@gmail.com>
2021-02-07 22:00:06 -0700

    Add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import './Footer.css'
 import { Button } from './Button'
+import { Link } from 'react-router-dom'
+
 
 function Footer() {
   return (
@@ -18,6 +21,18 @@ function Footer() {
           </form>
         </div>
       </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
