d18cdc75b6ce4c027561dbb022acd4ee69e5ad04
James Hooper <jameshooperdesigns@gmail.com>
2021-02-07 21:45:32 -0700

    Add the subscribe form to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from './Button'
 
 function Footer() {
   return (
@@ -10,6 +11,12 @@ function Footer() {
         <p className="footer-subscription-text">
           You can unsubscribe at any time.
         </p>
+        <div className="input-areas">
+          <form>
+            <input type="email" name="email" placeholder="Your Email" className="footer-input" />
+            <Button buttonStyle="btn--outline">Subscribe</Button> 
+          </form>
+        </div>
       </section>
     </div>
   )
