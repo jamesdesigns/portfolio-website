ba0e8f53c0b232a54ea3b14ac78ee0d5768ca6b5
James Hooper <jameshooperdesigns@gmail.com>
2021-02-08 15:21:59 -0700

    Edit the header in the Cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ import CardItem from './CardItem'
 function Cards() {
   return (
     <div className="cards">
-      <h1>Learn how to build your own beautiful websites.</h1>
+      <h1>Learn how to build your own beautiful website.</h1>
       <div className="cards__contianer">
         <div className="cards__wrapper">
           <ul className="cards__items">
