6de86bf6852f7a9c74319687ee2c648f6fa0ad42
James Hooper <jameshooperdesigns@gmail.com>
2021-02-07 21:40:20 -0700

    Add new Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join my design newsletter to receive discounts on my educational services.
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
