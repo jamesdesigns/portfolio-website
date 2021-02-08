cde8087679b9ccfa9483fd804133b61ad271bb53
James Hooper <jameshooperdesigns@gmail.com>
2021-02-07 21:32:57 -0700

    Organize the Card component items and add appropriate images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -12,13 +12,19 @@ function Cards() {
           <ul className="cards__items">
 
             <CardItem
-              src="images/img-9.jpg"
+              src="images/img-2.jpg"
               text="Tip Calculator that works as a tip and pay using the QR Scan code."
               label="Tip Calculator App"
               path="/about"
             />
             <CardItem
-              src="images/img-2.jpg"
+              src="images/img-home.jpg"
+              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              label="Restuarant Website"
+              path="/blog"
+            />
+            <CardItem
+              src="images/img-8.jpg"
               text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
               label="Restuarant Website"
               path="/portfolio"
@@ -26,22 +32,16 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-3.jpg"
+              src="images/img-4.jpg"
               text="Tip Calculator that works as a tip and pay using the QR Scan code."
               label="Tip Calculator App"
               path="/contact"
             />
             <CardItem
-              src="images/img-4.jpg"
+              src="images/img-9.jpg"
               text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
               label="Restuarant Website"
               path="/sign-up"
-            />
-                      <CardItem
-              src="images/img-8.jpg"
-              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
-              label="Restuarant Website"
-              path="/blog"
             />
 
           </ul>
