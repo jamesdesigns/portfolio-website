f166de70a091196d0a53599e657ec71c039f798f
James Hooper <jameshooperdesigns@gmail.com>
2021-02-08 12:06:29 -0700

    Add content to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,26 +6,26 @@ import CardItem from './CardItem'
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check Out My Best Work</h1>
+      <h1>Hello. I am here to help build great websites.</h1>
       <div className="cards__contianer">
         <div className="cards__wrapper">
           <ul className="cards__items">
 
             <CardItem
               src="images/img-2.jpg"
-              text="Tip Calculator that works as a tip and pay using the QR Scan code."
+              text="I have been designing for over 10 years for clients and love designing and building websites."
               label="My Story"
               path="/about"
             />
             <CardItem
               src="images/img-home.jpg"
-              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              text="Learn how to design websites that attract more users and help them become more engaged."
               label="How to Design and Build websites"
               path="/blog"
             />
             <CardItem
               src="images/img-8.jpg"
-              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              text="Explore my best design work I have done over the last year."
               label="Web Design Portfolio"
               path="/portfolio"
             />
@@ -33,13 +33,13 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="images/img-4.jpg"
-              text="Tip Calculator that works as a tip and pay using the QR Scan code."
+              text="Reach out to me if you are interested in a website. I offer creative and affordable solutions."
               label="Contact James Hooper"
               path="/contact"
             />
             <CardItem
               src="images/img-9.jpg"
-              text="Sushi Restaurant that allows the restaurant owner to update their inventory live."
+              text="Sign up today if you want to learn how to grow as a designer and developer."
               label="Sign Up and Start Learning Today!"
               path="/sign-up"
             />
