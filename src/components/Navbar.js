6f2f610e6c8f7058bc655db6753c1d9c777326bc
James Hooper <jameshooperdesigns@gmail.com>
2021-02-08 14:11:28 -0700

    Remove logo title and this will be replaced by a logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          <i className="fab fa-artstation"></i>&nbsp;
+          <i className="fab fa-artstation"></i>&nbsp; 
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
