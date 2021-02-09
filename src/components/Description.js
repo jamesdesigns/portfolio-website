ee571d38b67abb4c180a6c0ec08fa9f8e6575d91
James Hooper <jameshooperdesigns@gmail.com>
2021-02-09 16:44:19 -0700

    Edit content of my description

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ function Description() {
   return (
     <div className="description">
       <h1 className="role"><span className="fixQuote">&#x0201C;</span>IMAGINATION<span className="fixQuote">&#x0201D;</span></h1>
-      <p className="thoughts">What makes the web development field so exciting to me, is that technology is always improving and changing the way we see and interact with the web. I love a good challenge and love finding innovative ways to help improve the success of your business. Everyday I look forward to helping small and large businesses create beautiful, meaningful, and inspiring work that will help improve the quality of your brand.</p>
+      <p className="thoughts">What sets me apart is my creative imagination and experience in graphic design. My ability to design and develop these ideas continue to flourish. I have been creative my whole life and continue to leave an impression with everything that I create. I love a good challenge and love finding innovative ways to help improve the success of your business. Everyday I look forward to helping small and large businesses create beautiful, meaningful, and inspiring work that will help improve the quality of your brand.</p>
     </div>
   )
 }
