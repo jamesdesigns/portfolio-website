081327f00d00e6ba9137ab59b656c252d41d31b3
James Hooper <jameshooperdesigns@gmail.com>
2021-02-10 13:12:26 -0700

    Edit the content of the Description component

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,8 +4,8 @@ import './Description.css'
 function Description() {
   return (
     <div className="description">
-      <h1 className="role"><span className="fixQuote">&#x0201C;</span>IMAGINATION<span className="fixQuote">&#x0201D;</span></h1>
-      <p className="thoughts">What sets me apart is my creative imagination and experience in graphic design. My ability to design and develop new and exciting concepts continue to flourish. I have been creative my entire life and continue to leave a lasting impression. I love a good challenge and love finding innovative ways to help improve the success of your business. Everyday I look forward to helping small and large businesses create beautiful, meaningful, and inspiring work that will help improve the quality of your brand.</p>
+      <h1 className="role"><span className="fixQuote">&#x0201C;</span>CUTTING EDGE<span className="fixQuote">&#x0201D;</span></h1>
+      <p className="thoughts">What sets me apart is my creative imagination and experience as a graphic designer. My ability to design and develop new and exciting websites continue to leave a lasting impression. I love a good challenge and love finding innovative ways to help improve the success of your business. Everyday I look forward to helping businesses create cutting edge, meaningful, and inspiring work that will help improve your bottom line.</p>
     </div>
   )
 }
