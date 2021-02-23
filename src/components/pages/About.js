61b431a2db595f5535a489015b5086a44c744381
James Hooper <jameshooperdesigns@gmail.com>
2021-02-22 23:19:02 -0700

    Relocate sections in the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,12 +7,53 @@ export default function About() {
 
    return (
     <>
+    <div className="hero-container">
+      <video src="/videos/video-4.mp4" autoPlay loop muted />
+      <h1>James Hooper</h1>
+      <p>I work hard to create design work that will leave a lasting impression.</p>
+    </div>
     <div className="bio-container">
     {/* <h1 className="about">Hello! I am James Hooper.</h1> */}
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      {/* <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" /> */}
 
     </div>
-    <div className="about-page">
+   
+    <section className="black-bg">
+      <div className="myStory">
+      <h3 className="large-font-100 center-align pad-btm-20 gradient-text"><span>&#8595;</span><br /><br />I design websites.</h3>
+      <p>Hi, my name is James Hooper and I love designing professional work for clients. I have been creative my entire life, which ultimately led me to my passion with web design and development. After I earned a bachelors degree in Graphic Design from the University of Utah, I discovered that I wanted to learn everything there was to designing, building, and creating engaging websites.</p>
+      <br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <br />
+      <br />
+      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      <br /><br />
+      <h3 className="large-font-100 center-align pad-btm-20 gradient-text"><span>&#8595;</span></h3>
+      <br /><br />
+      <h3 className="large-font-100 center-align pad-btm-20 gradient-text">I create logos.</h3>
+      <br />
+      <br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <br />
+      <br />
+      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      <br /><br />
+      <h3 className="large-font-100 center-align pad-btm-20 gradient-text"><span>&#8595;</span></h3>
+      <br /><br />
+      <h3 className="large-font-100 center-align pad-btm-20 gradient-text">Music Aficionado.</h3>
+      <br />
+      <br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <br />
+      <br />
+      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
+      <br />
+      <br />
+      </div>
+      <div className="about-page">
       <div className="about-container">
         <h2 className="pad-btm-20">Education</h2>
         <ul style={{ listStyleType: "none"}}>
@@ -59,41 +100,6 @@ export default function About() {
         </ul>
       </div>
     </div>
-    <section className="black-bg">
-      <div className="myStory">
-      <h3 className="large-font-100 center-align pad-btm-20 gradient-text"><span>&#8595;</span><br /><br />I design websites.</h3>
-      <p>Hi, my name is James Hooper and I love designing professional work for clients. I have been creative my entire life, which ultimately led me to my passion with web design and development. After I earned a bachelors degree in Graphic Design from the University of Utah, I discovered that I wanted to learn everything there was to designing, building, and creating engaging websites.</p>
-      <br />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <br />
-      <br />
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
-      <br /><br />
-      <h3 className="large-font-100 center-align pad-btm-20 gradient-text"><span>&#8595;</span></h3>
-      <br /><br />
-      <h3 className="large-font-100 center-align pad-btm-20 gradient-text">I design logos.</h3>
-      <br />
-      <br />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <br />
-      <br />
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
-      <br /><br />
-      <h3 className="large-font-100 center-align pad-btm-20 gradient-text"><span>&#8595;</span></h3>
-      <br /><br />
-      <h3 className="large-font-100 center-align pad-btm-20 gradient-text">A Mountain Man.</h3>
-      <br />
-      <br />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <br />
-      <br />
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
-      <img className="james-bg" src="https://artistjameshooper.com/img/james-hooper-img.jpg" alt="James Hooper" />
-      <br />
-      <br />
-      </div>
     </section>
     <Footer />
     </>
