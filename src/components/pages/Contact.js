97967054674e1ca49c7d4bed0731380d3509531e
James Hooper <jameshooperdesigns@gmail.com>
2021-02-23 15:41:57 -0700

    Add css styles to the Contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,12 +7,14 @@ export default function Contact() {
     <>
     
     <div className="contact">
-      <div>
-        <h1>CONTACT</h1>
-        <p>Email: jameshooperdesigns@gmail.com</p>
-        <p>Phone: (801)718-9727</p>
+      <div className="contact-title">
+        <p className="large-font-100 heavy-font-weight">CONTACT</p>
+        <p className="medium-font-30"><b>Email:</b> &nbsp;  jameshooperdesigns@gmail.com</p>
+        <p className="medium-font-30"><b>Text:</b>  &nbsp;&nbsp;&nbsp;&nbsp;  <a className="white-text" href="sms:+18017189727&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for...">801-718-9727</a></p>
+        <br />
+        <h2>What's your name?</h2>
+        <input type="text" id="name" name="name" />
       </div>
-      <h2>What's your name?</h2>
     </div>
     <Footer />
     </>
