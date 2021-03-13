06b94fa0266c46ff05f263a5341cb2f4a7b71450
James Hooper <jameshooperdesigns@gmail.com>
2021-03-12 21:42:28 -0700

    Add a form to the contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
         <p className="medium-font-30"><b>Email:</b> &nbsp;  jameshooperdesigns@gmail.com</p>
         <p className="medium-font-30"><b>Text:</b>  &nbsp;&nbsp;&nbsp;&nbsp;  <a className="white-text" href="sms:+18017189727&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for...">801-718-9727</a></p>
         <br />
-				<div className="contact-container">
+				<form className="contact-container">
 					<h2>{greeting}</h2>
 					<input className="contact-name" type="text" id="name" name="name" />
 					<br /><br /><br /><br />
@@ -31,7 +31,7 @@ export default function Contact() {
 					<h2>Describe how you imagine your new website</h2>
 					<input className="contact-name" type="email" id="address" name="email" />
 					<br /><br /> */}
-				</div>
+				</form>
       </div>
     </div> 
     <Footer />
