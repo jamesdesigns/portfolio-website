0d9f39c6e390b3b6f6b8d5df9819d1f773201e74
James Hooper <jameshooperdesigns@gmail.com>
2021-03-12 21:38:39 -0700

    Add the variable greeting to the contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,6 +4,9 @@ import Footer from '../Footer'
 // import '../Component.css'
 
 export default function Contact() {
+
+	const greeting = `Hello! What's your name?`;
+	
   return (
     <>
     <div className="contact">
@@ -13,7 +16,7 @@ export default function Contact() {
         <p className="medium-font-30"><b>Text:</b>  &nbsp;&nbsp;&nbsp;&nbsp;  <a className="white-text" href="sms:+18017189727&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for...">801-718-9727</a></p>
         <br />
 				<div className="contact-container">
-					<h2>Hello! What's your name?</h2>
+					<h2>{greeting}</h2>
 					<input className="contact-name" type="text" id="name" name="name" />
 					<br /><br /><br /><br />
 					{/* <h2>What's the best way to contact you?</h2>
