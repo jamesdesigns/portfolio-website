e828f67487fae24e03fe2f45371aa77e7c1806f2
James Hooper <jameshooperdesigns@gmail.com>
2021-03-12 22:53:47 -0700

    Add selection of hex colors in the contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,8 +7,9 @@ import { useState } from 'react'
 export default function Contact() {
 
 	const greeting = `Hello! What's your name?`;
+	const COLORS = ['#588c75','#b0c47f','#f3e395','#f3ae73','#da645a','#79a38f','#c1d099','#f5eaaa','#f5be8f','#e1837b','#9bbaab','#d1dcb2','#f9eec0','#f7cda9'];
 	const [ values, setValues ] = useState({
-		name: '', email: '', phone:'', priority: '', color: '',
+		name: '', email: '', phone:'', priority: '', color: '', goal: ''
 	});
 	// const onChange = (event) => {
 	// 	setValue(event.target.value);
@@ -19,6 +20,29 @@ export default function Contact() {
 			setValues(oldValues => ({...oldValues, [name]: value }));
 		}
 	}
+
+	const saveFormData = async () => {
+		const repsonse = await fetch('/api/registration', {
+			method: 'POST',
+			body: JSON.stringify(values)
+		});
+		if (Response.status !== 200) {
+			throw new Error('Request failed: ${response.status}');
+		}
+	}
+
+	const onSubmit = async (event) => {
+		event.preventDefault(); // Prevent default submission
+		try {
+			await saveFormData();
+			alert('Your registration was successfully submitted!');
+			setValues({
+				name: ''
+			});
+		} catch (e) {
+			alert(`Registration failed! ${e.message}`);
+		}
+	}
   return (
     <>
     <div className="contact">
@@ -27,10 +51,10 @@ export default function Contact() {
         <p className="medium-font-30"><b>Email:</b> &nbsp;  jameshooperdesigns@gmail.com</p>
         <p className="medium-font-30"><b>Text:</b>  &nbsp;&nbsp;&nbsp;&nbsp;  <a className="white-text" href="sms:+18017189727&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for...">801-718-9727</a></p>
         <br />
-				<form className="contact-container">
+				<form onSubmit={onSubmit} className="contact-container">
 					<label><h2>{greeting}</h2></label>
 					<input value={values.name} onChange={set('name')} className="contact-name" type="text" id="name" name="name" required />
-					{/* <p><small>{values.name}</small></p> */}
+					<p><small>{values.name}</small></p>
 					<br /><br /><br /><br />
 					
 					{/* <label><h2>What's the best way to contact you?</h2></label>
@@ -45,13 +69,17 @@ export default function Contact() {
 					<p><small>{values.priority}</small></p>
 					<br /><br />
 					<label><h2>Choose a color for your website.</h2></label>
-					<input className="contact-name" type="color" id="color" name="color" />
+					<select>
+						<option value="">Select Color</option>
+						{COLORS.map(c => <option key={c}>{c}</option>)}
+					</select>
 					<p><small>{values.color}</small></p>
 					<br /><br />
 					<label><h2>Describe how you imagine your new website</h2></label>
 					<input className="contact-name" type="goal" id="goal" name="goal" />
 					<p><small>{values.goal}</small></p>
 					<br /><br />  */}
+					<button type="submit">SUBMIT</button>
 				</form>
       </div>
     </div> 
