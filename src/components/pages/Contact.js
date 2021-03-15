fc642ab9560e6cbf8afd0b55b1321ff6af8530b3
James Hooper <jameshooperdesigns@gmail.com>
2021-03-15 17:44:28 -0600

    Add new titles to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -47,7 +47,7 @@ export default function Contact() {
     <>
     <div className="contact">
       <div className="contact-title">
-        <p className="heavy-font-weight">CONTACT</p>
+        <p className="large-font-75 heavy-font-weight">CONTACT</p>
         <p className="medium-font-30"><b>Email:</b> &nbsp;  jameshooperdesigns@gmail.com</p>
         <p className="medium-font-30"><b>Text:</b>  &nbsp;&nbsp;&nbsp;&nbsp;  <a className="white-text" href="sms:+18017189727&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for...">801-718-9727</a></p>
         <br />
@@ -59,7 +59,7 @@ export default function Contact() {
 						<input value={values.email} onChange={set('email')} className="contact-name" type="email" id="email" name="email" required />
 						<label><p className="form-text-med">Phone:</p></label>
 						<input value={values.phone} onChange={set('phone')} className="contact-name" type="phone" id="phone" name="phone" />
-						<label><p className="form-text-med">Comments/Questions:</p></label>
+						<label><p className="form-text-med">How can I help you today?</p></label>
 						<input value={values.goal} onChange={set('goal')} className="contact-name" type="goal" id="goal" name="goal" required />
 					</div>
 					<div className="output-form">
