701a17dba51f0d14ce4ec2ffcb2fb798e79f031e
James Hooper <jameshooperdesigns@gmail.com>
2021-03-25 10:14:21 -0600

    Add new image of laptop

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -18,7 +18,7 @@ function Cards() {
               path="/blog"
             />
             <CardItem
-              src="images/laptop.jpg"
+              src="images/laptop-2.jpg"
               text="I have been designing for over 10 years for clients and love designing and building websites."
               label="My Story"
               path="/about"
