413fd8ff74d19083feeb6fe94a799e4f2332f361
James Hooper <jameshooperdesigns@gmail.com>
2021-04-01 17:25:10 -0600

    Add responsiveness to the images in the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -21,8 +21,10 @@ export default function About() {
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <br />
       <br />
-      <img className="james-bg" src="images/Outdoors-2.jpg" alt="High Uintas" /><br />
-      <img className="james-bg" src="images/Outdoors.jpg" alt="High Uintas" />
+      <div className="pic-1">
+        <img className="james-bg" src="images/Outdoors-2.jpg" alt="High Uintas" /><br />
+        <img className="james-bg" src="images/Outdoors.jpg" alt="High Uintas" />
+      </div>
       <br /><br />
 
       <br /><br />
