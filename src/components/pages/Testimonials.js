9a3f525eb15493c274f762379d759331d37b7dc3
James Hooper <jameshooperdesigns@gmail.com>
2021-04-13 15:24:02 -0600

    Add similar styles to the testimonials profile image

--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -10,16 +10,23 @@ export default function Testimonials() {
   return (
     <>
       <div className="testimonials">
-        <h1 className="margin-top-30 med-font-45">Satisfaction is Guaranteed</h1>
+        <h1 className="margin-top-30 med-font-45 gradient-text">Satisfaction is Guaranteed.</h1>
         <div className="testimonial-container">
-          <img className="james" width="200" height="200" src="images/client-bigmountainbarbell.png" alt="Lynndsey - Big Mountain Barbell" />
+
+        <div className="module-border-wrap profile-test">
+        <div className="module">
+          <img className="jamesProfileImg" src="images/client-bigmountainbarbell.png" alt="James Hooper" width="300" height="300" />
+        </div>
+      </div>
+
+          {/* <img className="james" width="200" height="200" src="images/client-bigmountainbarbell.png" alt="Lynndsey - Big Mountain Barbell" /> */}
           <blockquote className="selfQuote">
             <span className="fixQuote">“</span>
             James was a great designer and he considered
             everything that we were looking for in a new logo. I
             highly recommend him!
             <br />
-            <br />
+        
             <div className="test">
               - Lynndsey Eldridge, 
               <a className="testimonialLink" href="https://www.bigmountainbarbell.com" target="_blank">
