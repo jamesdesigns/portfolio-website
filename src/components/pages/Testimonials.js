01cc25dd7d75bb62770d436eb5a222dbc46f3c46
James Hooper <jameshooperdesigns@gmail.com>
2021-04-13 00:15:54 -0600

    Add styles to the new testimomials page

--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -9,7 +9,24 @@ export default function Testimonials() {
 
   return (
     <>
-      <h1 className="test">Testimonials</h1>
+      <div className="testimonials">
+        <h1 className="margin-top-30 med-font-45">Testimonials</h1>
+        <div className="testimonial-container">
+          <img className="james" width="200" height="200" src="img/client-bigmountainbarbell.png" alt="Lynndsey - Big Mountain Barbell" />
+          <blockquote className="selfQuote">
+            <span className="fixQuote">“</span>
+            James was a great designer and he considered
+            everything that we were looking for in a new logo. I
+            highly recommend him!
+            <br />
+            <div className="test">
+              - Lynndsey Eldridge, 
+              <a className="testimonialLink" href="https://www.bigmountainbarbell.com" target="_blank">
+              Big Mountain Barbell</a>
+            </div>
+          </blockquote>
+        </div>
+      </div>
       <Footer />
     </>
   )
