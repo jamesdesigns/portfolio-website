245dab33cff29b877b637dc07812d84df26c217d
James Hooper <jameshooperdesigns@gmail.com>
2021-04-15 16:08:59 -0600

    Remove extra portfolio row of empty projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -12,14 +12,9 @@ export default function Portfolio() {
         <div className="img-card"><a href="https://epic-shockley-7f65af.netlify.app/#" target="_blank"><img src="/images/ava-romero-website.jpg" alt="Real Estate Agent Website" width="350" height="250" /></a></div>
         <div className="img-card"><a href="https://artistjameshooper.com" target="_blank"><img src="/images/artistjameshooper-website.jpg" alt="Artist James Hooper Website" width="350" height="250" /></a></div>
       </div>
-      <div className="container-card">
+      <div className="container-card-2">
         <div className="img-card"><a href="https://5fbdf9e6c22f9d947029b895--agitated-payne-af112e.netlify.app/" target="_blank"><img src="/images/react-tip-calculator.jpg" alt="Tip Calculator" width="350" height="250" /></a></div>
         <div className="img-card"><a href="#" target="_blank"><img src="/images/validation-design-guide.jpg" alt="Validation Design Guide for Utah's Backcountry Website" width="350" height="250" /></a></div>
-        <div className="img-card"></div>
-      </div>
-      <div className="container-card-2">
-        <div className="img-card"></div>
-        <div className="img-card"></div>
         <div className="img-card"></div>
       </div>
     </section>
