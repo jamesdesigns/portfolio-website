b8828ec82cdc74e14aada1304025e3e71148909f
James Hooper <jameshooperdesigns@gmail.com>
2022-01-03 00:27:26 -0700

    Fix class name

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ function Cards() {
   return (
     <div className="cards">
       <h1 className="thin-font-weight">Learn how you can build beautiful websites.</h1>
-      <div className="cards__contianer">
+      <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
 
