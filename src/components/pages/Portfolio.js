3d84dcade1badc6e02eac8828a77edaac2df36bd
James Hooper <jameshooperdesigns@gmail.com>
2022-01-06 22:59:46 -0700

    Add new content to templates page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,7 +5,7 @@ import Footer from '../Footer'
 export default function Portfolio() {
   return (
     <div>
-    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px' }}>Website Templates</h1>
+    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px', marginTop: '90px' }}>Real Estate Websites</h1>
     <section>
       <div className="container-card">
         <div className="img-card"><a href="/" target="_blank"><img src="/images/personal-website-design.jpg" alt="James Hooper Designs Website" width="350" height="250" /></a></div>
@@ -18,7 +18,7 @@ export default function Portfolio() {
         <div className="img-card"><a href="https://artistjameshooper.com/dgm2780/index.html" rel="noreferrer"><img src="/images/calmmind-website.png" alt="Bootstrap Yoga Website" width="350" height="225" /></a></div>
       </div>
     </section>
-    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px' }}>Logo Templates</h1>
+    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px' }}>Portfolio Websites</h1>
     <section>
       <div className="container-card">
         <div className="img-card"><a href="/" target="_blank"><img src="/images/personal-website-design.jpg" alt="James Hooper Designs Website" width="350" height="250" /></a></div>
@@ -31,7 +31,20 @@ export default function Portfolio() {
         <div className="img-card"><a href="https://artistjameshooper.com/dgm2780/index.html" rel="noreferrer"><img src="/images/calmmind-website.png" alt="Bootstrap Yoga Website" width="350" height="225" /></a></div>
       </div>
     </section>
-    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px' }}>Business Card Templates</h1>
+    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px' }}>Photography Websites</h1>
+    <section>
+      <div className="container-card">
+        <div className="img-card"><a href="/" target="_blank"><img src="/images/personal-website-design.jpg" alt="James Hooper Designs Website" width="350" height="250" /></a></div>
+        <div className="img-card"><a href="https://epic-shockley-7f65af.netlify.app/#" rel="noreferrer"><img src="/images/ava-romero-website.jpg" alt="Real Estate Agent Website" width="350" height="250" /></a></div>
+        <div className="img-card"><a href="https://artistjameshooper.com" rel="noreferrer"><img src="/images/artistjameshooper-website.jpg" alt="Artist James Hooper Website" width="350" height="250" /></a></div>
+      </div>
+      <div className="container-card-2">
+        <div className="img-card"><a href="https://5fbdf9e6c22f9d947029b895--agitated-payne-af112e.netlify.app/" rel="noreferrer"><img src="/images/react-tip-calculator.jpg" alt="Tip Calculator" width="350" height="250" /></a></div>
+        <div className="img-card"><a href="/images/JamesHooper-Validation-Design-Guide-Final.pdf" rel="noreferrer" target="_blank"><img src="/images/validation-design-guide.jpg" alt="Validation Design Guide for Utah's Backcountry Website" width="350" height="250" /></a></div>
+        <div className="img-card"><a href="https://artistjameshooper.com/dgm2780/index.html" rel="noreferrer"><img src="/images/calmmind-website.png" alt="Bootstrap Yoga Website" width="350" height="225" /></a></div>
+      </div>
+    </section>
+    <h1 className="portfolio thin-font-weight" style={{fontSize: '35px' }}>Restaurant Websites</h1>
     <section>
       <div className="container-card">
         <div className="img-card"><a href="/" target="_blank"><img src="/images/personal-website-design.jpg" alt="James Hooper Designs Website" width="350" height="250" /></a></div>
