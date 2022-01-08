7206b923b01365edb1fd9407e779e86fe05f2f23
James Hooper <jameshooperdesigns@gmail.com>
2022-01-07 19:45:46 -0700

    Add new changes to project

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -9,13 +9,18 @@ const ContactMe = () => {
   const [message, setMessage] = useState('');
   const [emailSent, setEmailSent] = useState(false);
 
+
     const submit = () => {
       if (name && email && message) {
         // TODO - Send mail
 
-        const serviceId = 'service_id';
-        const templateId = 'template_id';
-        const userId = 'user_id';
+        const USERID = process.env.REACT_APP_API_KEY_USERID;
+        const TEMPLATEID = process.env.REACT_APP_TEMPLATEID;
+        const SERVICEID = process.env.REACT_APP_SERVICEID;
+
+        const serviceId = SERVICEID;
+        const templateId = TEMPLATEID;
+        const userId = USERID;
         const templateParams = {
           name,
           email,
@@ -29,10 +34,6 @@ const ContactMe = () => {
         setMessage('');
         setEmailSent(true);
       } else {
-      //   const isValidEmail = email => {
-      //     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      //     return regex.test(String(email).toLowerCase());
-      // };
         alert('Oops. Please fill out all the fields.');
       }
     }
@@ -46,7 +47,6 @@ const ContactMe = () => {
     <input className="contact-name"  type="email" placeholder="Email Address" value={email} onChange={e => setEmail(e.target.value)} />
     <textarea className="contact-name"  placeholder="Message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
     <button className="btn btn--outline btn--large btn-submit mar-btm-60" onClick={submit}>Send Message</button>
-    {/* <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch with you in no time!</span><br /><br /><br /> */}
     <span className={emailSent ? 'visible' : null}><span className="hidden">Thank you for your message, we will be in touch soon!</span></span><br /><br /><br />
 
   </div>
